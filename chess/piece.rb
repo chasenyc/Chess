a15a710b259c395924ada0a764e72ba3172c5621
Dan and Alex <danandalex@appacademy.io>
2015-10-05 18:04:02 -0400

    Implement piece movement and subclasses of Piece.

--- chess/piece.rb
+++ chess/piece.rb
@@ -11,6 +11,32 @@ class Piece
     :king   => "\u265a",
   }
 
+  STRAIGHT_STEP = [
+    [ 0,  1],
+    [ 0, -1],
+    [ 1,  0],
+    [-1,  0]
+  ]
+
+  DIAGONAL_STEP = [
+    [ 1,  1],
+    [ 1, -1],
+    [-1,  1],
+    [-1, -1]
+  ]
+
+  KNIGHT_STEP = [
+    [ 1,  2],
+    [ 1, -2],
+    [-1,  2],
+    [-1, -2],
+    [ 2,  1],
+    [ 2, -1],
+    [-2,  1],
+    [-2, -1]
+  ]
+
+  attr_reader :color
   attr_accessor :pos
 
   def initialize(board, type, color, pos)
@@ -30,51 +56,79 @@ end
 class SlidingPiece < Piece
   def moves(*direction)
     result = []
-    result += straight_moves if direction.include?(:straight)
-    result += diagonal_moves if direction.include?(:diagonal)
-  end
+    result += valid_moves(STRAIGHT_STEP) if direction.include?(:straight)
+    result += valid_moves(DIAGONAL_STEP) if direction.include?(:diagonal)
 
-  def straight_moves
-    result = []
-    8.times do |col|
-      result << [pos[0], col] unless col == pos[1]
-    end
-    8.times do |row|
-      result << [row, pos[1]] unless row == pos[0]
-    end
     result
   end
 
-  def diagonal_moves
+  def valid_moves(move_steps)
     result = []
-    8.times do |i|
-      8.times do |j|
-          result << [i,j] if i+j == pos.inject(:+) ||
-                             i-j == pos.inject(:-)
+
+    move_steps.each do |step|
+      new_pos = [(step[0]+pos[0]),(step[1]+pos[1])]
+
+      while @board.in_bounds?(new_pos) &&
+           @board[new_pos].nil?
+        result << new_pos
+        new_pos = [(step[0]+new_pos[0]),(step[1]+new_pos[1])]
+      end
+
+      if @board[new_pos].nil?
+      else
+
+        result << new_pos if @board[new_pos].color != self.color
       end
     end
-    result.delete(pos)
+
+    result
   end
 end
 
 class Queen < SlidingPiece
 
+  def moves
+    super(:diagonal, :straight)
+  end
 end
 
 class Rook < SlidingPiece
 
+  def moves
+    super(:straight)
+  end
 end
 
 class Bishop < SlidingPiece
 
+  def moves
+    super(:diagonal)
+  end
 end
 
 class SteppingPiece < Piece
 
+  def moves(step_type)
+    result = []
+    result += valid_moves(KNIGHT_STEP) if step_type == :knight
+
+  end
+
+  def valid_moves(move_steps)
+    result = []
+    move_steps.each do |step|
+      new_pos = [(step[0]+pos[0]),(step[1]+pos[1])]
+      result << new_pos if @board.in_bounds?(new_pos) &&
+                (@board[new_pos].nil? || @board[new_pos].color != color)
+    end
+    result
+  end
 end
 
 class Knight < SteppingPiece
-
+  def moves
+    super(:knight)
+  end
 end
 
 class King < SteppingPiece
